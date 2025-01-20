89251c5d6c0a2f79473e118229ff84cb25cda4e6
chaser <fedenev.ilya.dm@gmail.com>
2025-01-20 20:37:11 +0700

    Add UI for daily task reset time

--- app/src/main/java/dev/chsr/todo/ui/screens/dailyTasksScreen/components/DailyTaskItem.kt
+++ app/src/main/java/dev/chsr/todo/ui/screens/dailyTasksScreen/components/DailyTaskItem.kt
@@ -2,7 +2,10 @@ package dev.chsr.todo.ui.screens.dailyTasksScreen.components
 
 import android.os.Build
 import androidx.annotation.RequiresApi
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -12,6 +15,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
@@ -82,14 +86,28 @@ fun DailyTaskItem(_task: Task, tasksViewModel: TasksViewModel) {
                 fontSize = 20.sp,
                 textAlign = TextAlign.Center
             )
-            Text(
-                modifier = Modifier.fillMaxWidth(),
-                text = taskStatusString.value,
-                color = task.status.color,
-                fontWeight = FontWeight.Bold,
-                fontSize = 12.sp,
-                textAlign = TextAlign.Center
-            )
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    text = taskStatusString.value,
+                    color = task.status.color,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 12.sp,
+                )
+                Text(
+                    modifier = Modifier.padding(start=16.dp),
+                    text = "Reset: ${
+                        LocalTime.of(task.resetTime.hour, task.resetTime.minute)
+                            .format(formatter)
+                    }",
+                    color = Color.Black,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 12.sp,
+                )
+            }
         }
     }
 }
